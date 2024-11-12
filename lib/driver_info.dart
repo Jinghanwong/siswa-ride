47bf7b132f815cb8125715543049608f558120df
Jinghanwong <122391666+Jinghanwong@users.noreply.github.com>
2024-11-12 20:34:08 +0800

    Create driver_info.dart

---
+++ lib/driver_info.dart
@@ -0,0 +1,20 @@
+// driver_info.dart
+class DriverInfo {
+  final String driverId;
+  final String name;
+  final String driverPhotoUrl;
+  final String carType;
+  final String carColour;
+  final String carPlateNumber;
+  final String bookingId;
+
+  DriverInfo({
+    required this.driverId,
+    required this.name,
+    required this.driverPhotoUrl,
+    required this.carType,
+    required this.carColour,
+    required this.carPlateNumber,
+    required this.bookingId,
+  });
+}
